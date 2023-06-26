7987323c7139766f26e651676a344dfc9d3091d9
pratik2820 <130150715+pratik2820@users.noreply.github.com>
2023-06-26 14:59:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,14 @@
-//your JS code here. If required.
-function test(){
-	// let prom = new Promise((resolve)=>{
-	// 	setTimeout(()=>{
-	// 		resolve(`Hello, world!`)
-	// 	},1000)
-	// })
-	// prom.then((data)=>{
-	// 	let text = document.getElementById("output");
-	// 	text.innerText = data;
-	// 	document.body.append(text)
-	// })
-	// let prom1 = new Promise((resolve)=>{
-	// 	setTimeout(()=>{
-	// 		let data = 'Hello, world!'
-	// 		resolve(data);
-	// 	},1000)
-	// })
-
-	// const output = document.getElementById("output");
-	// prom1.then((data)=>{
-	// 	output.innerText = data;
-	// })
-
+function generatePromise(){
 	return new Promise((resolve,reject)=>{
 		setTimeout(()=>{
 			resolve("Hello, world!")
 		},1000)
 	})
 }
+generatePromise().then((data)=>{
+	const element = document.getElementById("output");
+	element.innerText = data;
+})
 
-	test().then((data)=>{
-		const element = getElementById("output");
-		element.innerText = data;
-	})
 
 	
